06d5c911ea2e565b164058ecbad94fac735547ab
giorgosideriss <georgesideriss@yahoo.com>
2025-12-02 12:44:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const DATA_SOURCES = {
     "2017": "data/audi/2017/2017.json",
     "2018": "data/audi/2018/2018.json",
     "2019": "data/audi/2019/2019.json"
-  }
+  },
   "Abarth": {
     "2017": "data/abarth/2017/2017.json",
   }
